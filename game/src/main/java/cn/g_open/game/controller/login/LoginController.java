44abf6e78037a8da3c5909a50f55004ec887d5bd
hexing357013169 <hexing357013169@qq.com>
2017-07-11 23:08:53 +0800

    loginController

--- game/src/main/java/cn/g_open/game/controller/login/LoginController.java
+++ game/src/main/java/cn/g_open/game/controller/login/LoginController.java
@@ -4,7 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.g_open.game.common.dto.ResultDTO;
+import cn.g_open.game.common.utils.CommonUtil;
+import cn.g_open.game.common.utils.ResultMsg;
 
 @Controller
 @RequestMapping("login")
@@ -14,6 +19,12 @@ public class LoginController
     public void login(HttpServletRequest request,HttpServletResponse response){
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        
+        ResultDTO result = new ResultDTO();
+        if(StringUtils.isEmpty(username) || StringUtils.isEmpty(password))
+        {
+            result.setResultMsg(ResultMsg.ERR_LOGIN_EMPTY);
+            CommonUtil.responseMsg(response, result);
+            return;
+        }
     }
 }
